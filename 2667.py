d69461a66de3ae4ccac0b500ea84e349be46ba26
hanriverkitty <danieltls@naver.com>
2022-08-15 20:42:23 +0900

    20220815

--- 2667.py
+++ 2667.py
@@ -3,13 +3,14 @@ sys.setrecursionlimit(10**7)
 
 n = int(input())
 arr = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
-print(arr)
 cnt = 2 
 result=0
+#dfs 탐색
 def dfs(x,y,cnt):
     if(x<0 or x>=n or y<0 or y>=n):
         return False
     if(arr[x][y]==1):
+        #cnt로 각각 다른 단지 부여
         arr[x][y]=cnt
         dfs(x-1,y,cnt)
         dfs(x+1,y,cnt)
@@ -21,15 +22,19 @@ def dfs(x,y,cnt):
 for i in range(n):
     for j in range(n):
         if dfs(i,j,cnt) == True:
+            #깊이 탐색을 다한 후 결과값 증가 단지값 증가
             result+=1
             cnt+=1
 print(result)
+print(arr)
 a_cnt = []
 count=0
+#각각의 단지가 몇개 인지 탐색
 for i in range(cnt-1,1,-1):
     for j in range(n):
         count+=arr[j].count(i)
     a_cnt.append(count)
+    #0으로 초기화후 다른단지 재탐색
     count=0  
 a_cnt.sort()
 for i in a_cnt:
