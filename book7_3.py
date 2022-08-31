ecdaa6f9317e87d0b2fae57649fcda8dbe0456ef
hanriverkitty <danieltls@naver.com>
2022-08-31 12:09:22 +0900

    first book7_3

--- book7_3.py
+++ book7_3.py
@@ -5,7 +5,6 @@ arr1 = [ i for i in range(max(arr)+1)]
 a=0
 def binary_search(arr,arr1,start,end,m):
     if start>end:
-        print(start,end)
         return None
     mid = (start+end)//2
     sum = 0
@@ -15,16 +14,14 @@ def binary_search(arr,arr1,start,end,m):
    
     if sum==m:
         global a
-        print("gogo")
         a = arr1[mid]
-        print(a)
         return a
     elif sum>m:
-        binary_search(arr,arr1,mid+1,end,m)
+        return binary_search(arr,arr1,mid+1,end,m)
     else:
-        binary_search(arr,arr1,start,mid-1,m)
+        return binary_search(arr,arr1,start,mid-1,m)
         
-    return a
+    
     
     
 print(binary_search(arr,arr1,0,len(arr1)-1,m))
