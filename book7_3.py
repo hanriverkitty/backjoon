b0b538d9e506f2557d04496b50e0a6e87fda7011
hanriverkitty <danieltls@naver.com>
2022-08-31 17:26:54 +0900

    fix book7_3

--- book7_3.py
+++ book7_3.py
@@ -1,27 +1,24 @@
 import sys
 n,m = map(int,sys.stdin.readline().rstrip().split())
 arr = list(map(int,sys.stdin.readline().rstrip().split()))
-arr1 = [ i for i in range(max(arr)+1)]
-a=0
-def binary_search(arr,arr1,start,end,m):
+
+def binary_search(arr,start,end,m):
     if start>end:
-        return None
+        return end
     mid = (start+end)//2
     sum = 0
     for i in arr:
-        if(i>=arr1[mid]):
-            sum+=i-arr1[mid]
-   
-    if sum==m:
-        global a
-        a = arr1[mid]
-        return a
-    elif sum>m:
-        return binary_search(arr,arr1,mid+1,end,m)
+        if(i>=mid):
+            sum+=i-mid
+        if sum>m:
+            break
+    
+    if sum>=m:
+        return binary_search(arr,mid+1,end,m)
     else:
-        return binary_search(arr,arr1,start,mid-1,m)
+        return binary_search(arr,start,mid-1,m)
         
     
     
     
-print(binary_search(arr,arr1,0,len(arr1)-1,m))
+print(binary_search(arr,1,max(arr),m))
