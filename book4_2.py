e77e3c000e64c4074bc24bc6956049537b1577a9
hanriverkitty <danieltls@naver.com>
2022-11-10 14:16:37 +0900

    move textbook code

--- book4_2.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-count = 0
-for i in range(a+1):
-    for j in range(0, 60):
-        for k in range(0, 60):
-            if '3' in str(i)+str(j)+str(k):
-                count += 1
-                print(str(k))
