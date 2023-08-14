ea5306058f4385b3081bcc50afda276acacfe6c7
hanriverkitty <danieltls@naver.com>
2023-08-14 16:38:19 +0900

    2004

---
+++ 2004.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+a,b = map(int,input().split())
+
+if a-b<b:
+    b=a-b
+
+#팩토리얼에 들어가는 인수의 개수는 n!이라 했을때, n을 i의 지수승들로 나눈 몫들의 합
+# 10! 에 들어가는 2의 인수의 개수는 (10//2 = 5) + (10//4 = 2) + (10//8 = 1)
+def go(num,k):
+    count=0
+    div=1
+    while div<=num:
+        count+=num//div
+        div*=k
+    return count
+
+cnt_2 = go(a,2)-go(b,2)-go(a-b,2)
+cnt_5 = go(a,5)-go(b,5)-go(a-b,5)
+print(min(cnt_2,cnt_5))
--- 3036.py
+++ 3036.py
@@ -4,18 +4,15 @@ n = int(input())
 arr = list(map(int,input().split()))
 circle = arr[0]
 
-def sub(a):
-    c=1
-    count=1
-    while(count<=a):
-        if circle%count==0 and a%count==0:
-            c=count
-        count+=1
-    return c
+#유클리드 호제법
+def sub(a,b):
+    while (b>0):
+        a,b=b,a%b
+    return a
 
 for i in range(1,n):
     if circle%arr[i]!=0:
-        a = sub(arr[i])
+        a = sub(circle,arr[i])
         print(str(circle//a)+"/"+str(arr[i]//a))
     else:
         print(str(circle//arr[i])+"/"+str(1))
