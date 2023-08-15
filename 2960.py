26da33b732582a8136b5bfbd8ebefcd664ab53e0
hanriverkitty <danieltls@naver.com>
2023-08-15 15:26:46 +0900

    2960 + another answer

--- 2960.py
+++ 2960.py
@@ -16,3 +16,22 @@ for i in arr:
             break       
      
 print(arr1[k-1])
+
+
+###########
+"""
+n, k = map(int, input().split())
+
+data = [True for i in range(n + 1)]
+count = 0
+
+for i in range(2, len(data) + 1) :
+  for j in range(i, n+1, i) :
+    if data[j] :
+      data[j] = False
+      count += 1
+      if count == k :
+        print(j)
+        break
+"""
+##############
